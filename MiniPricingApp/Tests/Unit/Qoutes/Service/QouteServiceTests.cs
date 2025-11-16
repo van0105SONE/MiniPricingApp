2c8cbf09c364b167a8d03cf4beb2ceda6dde565c
van0105SONE <chanthakhaninsyxiengmai@gmail.com>
2025-11-17 01:25:37 +0700

    fixed: fixed  mismacht result data type

--- MiniPricingApp/Tests/Unit/Qoutes/Service/QouteServiceTests.cs
+++ MiniPricingApp/Tests/Unit/Qoutes/Service/QouteServiceTests.cs
@@ -94,7 +94,7 @@ namespace MiniPricingApp.Tests.Unit.Qoutes.Service
             var result = _service.CalculatePrice(request);
 
             // Assert
-            Assert.Equal(9.5, result.data.finalPrice);
+            Assert.Equal(9.5, Double.Parse(result.data.finalPrice.ToString()));
         }
 
 
@@ -125,7 +125,7 @@ namespace MiniPricingApp.Tests.Unit.Qoutes.Service
             var result = _service.CalculatePrice(request);
 
             // Assert
-            Assert.Equal(29.5, result.data.finalPrice);
+            Assert.Equal(29.5, Double.Parse(result.data.finalPrice.ToString()));
         }
 
 
